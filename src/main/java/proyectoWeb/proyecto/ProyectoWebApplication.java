e3136c02f33f81f2271c99b7195f20eeb2508b6d
luisd <luisd@LAPTOP-KGKKSANB>
2022-03-22 18:32:40 -0600

    Inicio

---
+++ src/main/java/proyectoWeb/proyecto/ProyectoWebApplication.java
@@ -0,0 +1,13 @@
+package proyectoWeb.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWebApplication.class, args);
+	}
+
+}
